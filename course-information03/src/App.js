f69b2d3a409b809586d8d1109f3cb01860f2deb3
GuiPolezi <gui.polezi@hotmail.com>
2025-12-17 13:24:01 -0300

    Initial commit part03

--- course-information03/src/App.js
+++ course-information03/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// Refatore o componente Content
+
 
 function App() {
+  console.log("lOG DO APP");
+
+  const header = "Desenvolvimento de aplicação Half Stack";
+  const exercises = [10, 7, 14];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header nome={header}/>
+      <Conteudo />
+      <Total total={exercises[0] + exercises[1] + exercises[2]}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+function Header(props) {
+  console.log(props);
+  return (
+    <>
+    <small>Nome do curso</small>
+    <h1> {props.nome} </h1>
+    </>
+  )
+}
+
+
+function Conteudo() {
+  return (
+    <div>
+      <Part parte="1 Fundamentos da biblioteca React" exercicio="10"/>
+      <Part parte="2 Usando props para passar dados" exercicio="7"/>
+      <Part parte="3 Estado de um componente" exercicio="14"/>
+    </div>
+  )
+}
+
+function Part(props) {
+  console.log(props);
+  return (
+    <>
+    <p>Parte: {props.parte} | Exercicio: {props.exercicio}</p>
+    </>
+  )
+}
+
+function Total(props) {
+  console.log(props)
+  return (
+    <>
+      <p>Total de exercícios: {props.total}</p>
+    </>
+  )
+}
