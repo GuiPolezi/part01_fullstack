81c621bcdbe1d419b470d49dcdbc0fc8c6d8d19a
GuiPolezi <gui.polezi@hotmail.com>
2025-12-17 12:51:14 -0300

    Iniciando segundo passo do exercicio Course Information

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -3,32 +3,7 @@
 // Header deve renderizar o nome do curso
 // Content renderiza as partes e número de exercícios
 // Total renderiza o total de exercicios.
-/*
-const App = () => {
-  const course = 'Desenvolvimento de aplicação Half Stack'
-  const part1 = 'Fundamentos da biblioteca React'
-  const exercises1 = 10
-  const part2 = 'Usando props para passar dados'
-  const exercises2 = 7
-  const part3 = 'Estado de um componente'
-  const exercises3 = 14
 
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-} */
 
 function App() {
   console.log("lOG DO APP");
