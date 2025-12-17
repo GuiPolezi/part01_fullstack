1459738874562a770be399b02e63a52f4731158c
GuiPolezi <gui.polezi@hotmail.com>
2025-12-17 14:49:26 -0300

    Adjusting Header Function

--- course-information05/src/App.js
+++ course-information05/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// Avançando para o uso de objetos
 
-function App() {
+const App = () => {
+  const course = {
+    name: 'Desenvolvimento de aplicação Half Stack',
+    parts: [
+      {
+        name: 'Fundamentos da biblioteca React',
+        exercises: 10
+      },
+      {
+        name: 'Usando props para passar dados',
+        exercises: 7
+      },
+      {
+        name: 'Estado de um componente',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course.name}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+function Header(props) {
+  console.log(props);
+  return (
+    <>
+    <small>Nome do curso</small>
+    <h1> {props.course} </h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p> part1: {props.parte[0].name} | exercises: {props.parte[0].exercises}</p>
+      <p> part2: {props.parte[1].name} | exercises: {props.parte[1].exercises}</p>
+      <p> part3: {props.parte[2].name} | exercises: {props.parte[2].exercises}</p>
+    </div>
+  )
+}
+
+
+function Total(props) {
+  console.log(props)
+  return (
+    <>
+      <p>Total de exercícios: {props.total}</p>
+    </>
+  )
+}
